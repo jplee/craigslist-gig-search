fc39cc1d88fae5f042fe77785cb1fc0753ce2988
Joe Lee <jpl@ireland.com>
2013-04-27 17:21:02 -0700

    Added license information.

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
 /*
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    This program is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+    along with this program.  If not, see http://www.gnu.org/licenses/.
+*/
 
 $location = "sfbay";
 $address = "http://www.craigslist.org/";
@@ -74,6 +74,32 @@ form select option
 }
 </style>
 
+<script>
+    /*    
+    @licstart  The following is the entire license notice for the 
+    JavaScript code in this page.
+
+    Copyright (C) 2012  Loic J. Duros
+
+    The JavaScript code in this page is free software: you can
+    redistribute it and/or modify it under the terms of the GNU
+    General Public License (GNU GPL) as published by the Free Software
+    Foundation, either version 3 of the License, or (at your option)
+    any later version.  The code is distributed WITHOUT ANY WARRANTY;
+    without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
+
+    As additional permission under GNU GPL version 3 section 7, you
+    may distribute non-source (e.g., minimized or compacted) forms of
+    that code without the copy of the GNU GPL normally required by
+    section 4, provided you include this license notice and a URL
+    through which recipients can access the Corresponding Source.
+
+
+    @licend  The above is the entire license notice for the 
+    JavaScript code in this page.
+    */
+</script>
 <script language="javascript" src="js/jquery-1.6.2.min.js"></script>
 <script language="javascript" src="js/jquery.dimensions.pack.js"></script>
 
